5151fc3d96dc1d92241acf935dcd105577383700
zhoufaming <847881570@qq.com>
2016-09-08 15:34:49 +0800

    Initial Commit

---
+++ inputPhoneNumber/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  inputPhoneNumber
+//
+//  Created by 周发明 on 16/9/8.
+//  Copyright © 2016年 周发明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ inputPhoneNumber/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  inputPhoneNumber
+//
+//  Created by 周发明 on 16/9/8.
+//  Copyright © 2016年 周发明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
